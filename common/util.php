18a07b9328c881c616707f690f3079fd47308057
David Callizaya <davidcallizaya@gmail.com>
2018-09-10 10:54:21 -0400

    Add deleteDir helper

--- common/util.php
+++ common/util.php
@@ -234,3 +234,18 @@ function strip_mq(){
       $stripslashes_deep($_REQUEST, $stripslashes_deep);
   }
 }
+function deleteDir($dirPath)
+{
+    if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
+        $dirPath .= '/';
+    }
+    $files = glob($dirPath . '*', GLOB_MARK);
+    foreach ($files as $file) {
+        if (is_dir($file)) {
+            deleteDir($file);
+        } else {
+            unlink($file);
+        }
+    }
+    rmdir($dirPath);
+}
